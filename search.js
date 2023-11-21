f83938b3f8e567b42fcda4ef356e39ef4781f4bd
unknown <biswasmahmuda5@gmail.com>
2023-11-20 21:34:01 -0800

    chack indexof or exists this string

--- search.js
+++ search.js
@@ -1,3 +1,9 @@
 const lyrix = "this is the most fancy version in the country";
 const include = lyrix.toLocaleLowerCase().includes("most".toLocaleLowerCase());
 console.log(include);
+
+if (lyrix.indexOf("more") !== -1) {
+  console.log("exists inside the string");
+} else {
+  console.log("not exists");
+}
