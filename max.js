d5971af8b6a0fc2dcc0f5ec2a037a3339f033ce2
unknown <biswasmahmuda5@gmail.com>
2023-11-22 17:55:15 -0800

    maximam number

---
+++ max.js
@@ -0,0 +1,12 @@
+const jim = 64;
+const dela = 76;
+const chinku = 98;
+
+if (jim > dela && jim > chinku) {
+  console.log("jim will be win");
+} else if (dela > jim && dela > chinku) {
+  console.log("dela will be win");
+} else {
+  console.log("chinku will be win");
+}
+console.log(Math.max(64, 76, 98));
